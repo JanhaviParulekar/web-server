0591973c48c89500bfdd05a59a3f5f9162ac53b5
Janhavi Parulekar <parulekar.j@husky.neu.edu>
2018-07-30 15:25:23 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    var log = `Today is ${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next()
+});
+
+app.use((req, res, next) =>{
+    res.render('maintenance.hbs')
+});
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getFullYear', () =>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+    // res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name: 'Janhavi',
+    //     likes: [
+    //         'Swimming',
+    //         'Cooking'
+    //     ]
+    // });
+
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hello World'
+    });
+});
+
+app.get('/about', (req, res) =>{
+    // res.send('About Page');
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req,res) =>{
+    res.send({
+        errorMessage : 'Error occured'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
